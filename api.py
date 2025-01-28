10db1dd11535c9297d774b072912d54c7cbc3786
fabrisdev <ezepuntocom@hotmail.com>
2025-01-28 10:26:03 -0600

    moved installer to api

--- api.py
+++ api.py
@@ -1,7 +1,16 @@
 import webview
+from lib import *
 
 class Api:
     def exit(self):
         webview.windows[0].destroy()
     def install(self):
-        pass
+        download_mod()
+        print('Finished downloading mod. Copying Reloaded Installation and installing the mod...')
+        extract_and_store_reloaded_installation()
+        try_delete_old_reloaded_configs()
+        try_delete_old_mod_versions()
+        extract_and_store_mod()
+        print('Finished installing the mod. Creating shortcut...')
+        create_shortcut()
+        print('All done!')
--- main.py
+++ main.py
@@ -1,17 +1,6 @@
-from lib import *
 import sys
 from window import start_gui
 
 
 start_gui()
 sys.exit(0)
-print("Downloading mod...")
-download_mod()
-print('Finished downloading mod. Copying Reloaded Installation and installing the mod...')
-extract_and_store_reloaded_installation()
-try_delete_old_reloaded_configs()
-try_delete_old_mod_versions()
-extract_and_store_mod()
-print('Finished installing the mod. Creating shortcut...')
-create_shortcut()
-print('All done!')
