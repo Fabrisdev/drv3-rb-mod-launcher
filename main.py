28e027b6e611bf6051d146bd768b970935026d10
fabrisdev <ezepuntocom@hotmail.com>
2025-01-28 00:53:24 -0600

    started ui development

--- main.py
+++ main.py
@@ -1,5 +1,42 @@
 from lib import *
+import webview
+import os
+from ctypes import windll
 
+def resource_path(relative_path):
+    try:
+        base_path = sys._MEIPASS
+    except Exception:
+        base_path = os.path.abspath(".")
+
+    return os.path.join(base_path, relative_path)
+
+class Api:
+    def exit(self):
+        webview.windows[0].destroy()
+
+def get_window_border_size():
+    user32 = windll.user32
+    screen = user32.GetSystemMetrics
+    border_width = screen(32)  # Ancho del borde
+    border_height = screen(33)  # Alto del borde
+    title_bar_height = screen(4)  # Altura de la barra de título
+    return border_width * 2, border_height * 2 + title_bar_height
+
+border_width, border_height = get_window_border_size()
+print(border_width, border_height)
+webview.create_window(
+    'Danganronpa V3: Resolute Rebellion', 
+    resource_path('src/index.html'),
+    js_api=Api(),
+    resizable=False,
+    width=1280 + border_width + 8,
+    height=720 + border_height + 8,
+)
+webview.start()
+
+
+exit(0)
 print("Downloading mod...")
 download_mod()
 print('Finished downloading mod. Copying Reloaded Installation and installing the mod...')
---
+++ src/script.js
@@ -0,0 +1,43 @@
+const installSelectedImage = document.getElementById('install_selected_image')
+const optionsSelectedImage = document.getElementById('options_selected_image')
+const exitSelectedImage = document.getElementById('exit_selected_image')
+const installHitbox = document.getElementById('install_hitbox')
+const optionsHitbox = document.getElementById('options_hitbox')
+const exitHitbox = document.getElementById('exit_hitbox')
+let selected = "install"
+
+installHitbox.addEventListener('mouseover', () => {
+    exitSelectedImage.style.visibility = 'hidden'
+    optionsSelectedImage.style.visibility = 'hidden'
+    installSelectedImage.style.visibility = 'visible'
+    if(selected === "install") return
+    playSelectSoundEffect()
+    selected = "install"
+})
+
+optionsHitbox.addEventListener('mouseover', () => {
+    exitSelectedImage.style.visibility = 'hidden'
+    installSelectedImage.style.visibility = 'hidden'
+    optionsSelectedImage.style.visibility = 'visible'
+    if(selected === "options") return
+    playSelectSoundEffect()
+    selected = "options"
+})
+
+exitHitbox.addEventListener('mouseover', () => {
+    installSelectedImage.style.visibility = 'hidden'
+    optionsSelectedImage.style.visibility = 'hidden'
+    exitSelectedImage.style.visibility = 'visible'
+    if(selected === "exit") return
+    playSelectSoundEffect()
+    selected = "exit"
+})
+
+function playSelectSoundEffect(){
+    const audio = new Audio("select_sound_effect.mp3")
+    audio.play()
+}
+
+exitHitbox.addEventListener('click', () => {
+    pywebview.api.exit()
+})
