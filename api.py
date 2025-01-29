99e12d1c15c9b57b3eac592d103e972563a6c2f6
fabrisdev <ezepuntocom@hotmail.com>
2025-01-28 22:01:57 -0600

    first popup made

--- api.py
+++ api.py
@@ -5,12 +5,17 @@ class Api:
     def exit(self):
         webview.windows[0].destroy()
     def install(self):
-        download_mod()
-        print('Finished downloading mod. Copying Reloaded Installation and installing the mod...')
-        extract_and_store_reloaded_installation()
-        try_delete_old_reloaded_configs()
-        try_delete_old_mod_versions()
-        extract_and_store_mod()
-        print('Finished installing the mod. Creating shortcut...')
-        create_shortcut()
-        print('All done!')
+        webview.windows[0].evaluate_js('showAlreadyExistingInstallationAlert()')
+        #download_mod()
+        #print('Finished downloading mod. Copying Reloaded Installation and installing the mod...')
+        #extract_and_store_reloaded_installation()
+        #try_delete_old_reloaded_configs()
+        #try_delete_old_mod_versions()
+        #extract_and_store_mod()
+        #print('Finished installing the mod. Creating shortcut...')
+        #create_shortcut()
+        #print('All done!')
+    def answered_yes(self):
+        print('Yes!')
+    def answered_no(self):
+        print('No')
--- src/script.js
+++ src/script.js
@@ -44,9 +44,61 @@ function playSelectSoundEffect(){
 }
 
 installHitbox.addEventListener('click', () => {
+    playSelectSoundEffect()
     pywebview.api.install()
 })
 
 exitHitbox.addEventListener('click', () => {
     pywebview.api.exit()
 })
+
+function showAlreadyExistingInstallationAlert(){
+    const warningNo = document.getElementById('warning_existing_installation_found_no')
+    const warningYes = document.getElementById('warning_existing_installation_found_yes')
+    const noHitbox = document.getElementById('no_hitbox')
+    const yesHitbox = document.getElementById('yes_hitbox')
+    installHitbox.style.visibility = 'hidden'
+    optionsHitbox.style.visibility = 'hidden'
+    exitHitbox.style.visibility = 'hidden'
+    warningNo.style.visibility = 'visible'
+    warningNo.classList.add('show_alert')
+    const noHitboxMouseover = function(){
+        warningYes.style.visibility = 'hidden'
+        warningNo.style.visibility = 'visible'
+        playHoverSoundEffect()
+    }
+    const yesHitboxMouseover = function(){
+        warningNo.style.visibility = 'hidden'
+        warningYes.style.visibility = 'visible'
+        playHoverSoundEffect()
+    }
+    noHitbox.addEventListener('mouseover', noHitboxMouseover)
+    yesHitbox.addEventListener('mouseover', yesHitboxMouseover)
+    getAnswer()
+        .then(() => pywebview.api.answered_yes())
+        .catch(() => pywebview.api.answered_no())
+        .finally(() => {
+            playSelectSoundEffect()
+            warningNo.style.visibility = 'hidden'
+            warningYes.style.visibility = 'hidden'
+            noHitbox.removeEventListener('mouseover', noHitboxMouseover)
+            yesHitbox.removeEventListener('mouseover', yesHitboxMouseover)
+            installHitbox.style.visibility = 'visible'
+            optionsHitbox.style.visibility = 'visible'
+            exitHitbox.style.visibility = 'visible'
+        })
+}
+
+function getAnswer(){
+    const noHitbox = document.getElementById('no_hitbox')
+    const yesHitbox = document.getElementById('yes_hitbox')
+    const answer = new Promise((resolve, reject) => {
+        noHitbox.addEventListener('click', () => {
+            reject()
+        })
+        yesHitbox.addEventListener('click', () => {
+            resolve()
+        })
+    })
+    return answer
+}
