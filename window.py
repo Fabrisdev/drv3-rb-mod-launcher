e86af37259208650ced9b55f13af800e99b73296
fabrisdev <ezepuntocom@hotmail.com>
2025-01-29 16:35:47 -0600

    Fixed window not taking into account scaling size of the screen

--- window.py
+++ window.py
@@ -2,13 +2,14 @@ import webview
 from lib import resource_path
 from api import Api
 from ctypes import windll
+windll.user32.SetProcessDPIAware()
 
 def get_window_border_size():
     user32 = windll.user32
     screen = user32.GetSystemMetrics
-    border_width = screen(32)  # Ancho del borde
-    border_height = screen(33)  # Alto del borde
-    title_bar_height = screen(4)  # Altura de la barra de título
+    border_width = screen(32)
+    border_height = screen(33)
+    title_bar_height = screen(4)
     return border_width * 2, border_height * 2 + title_bar_height
 
 def start_gui():
