9089aacbbcd4140a905c6370180bf9d8df0826e5
fabrisdev <ezepuntocom@hotmail.com>
2025-03-12 14:02:42 -0600

    Showing buttons

--- src/js/alert.js
+++ src/js/alert.js
@@ -1,7 +1,23 @@
 const alertImage = document.getElementById("alert-image")
 const alertText = document.getElementById('installation_started_text')
+const firstButton = document.getElementById('button-container-1')
+const secondButton = document.getElementById('button-container-2')
+const thirdButton = document.getElementById('button-container-3')
+const buttonIfOnlyOne = document.getElementById('button-container-if-only-one')
 
 export function showAlert({ text, buttons, isCancellable }){
     alertImage.style.visibility = 'visible'
     alertText.innerHTML = text
+    if(buttons.length === 1){
+        buttonIfOnlyOne.style.visibility = 'visible'
+    }
+    if(buttons.length > 1){
+        firstButton.style.visibility = 'visible'
+    }
+    if(buttons.length >= 2){
+        secondButton.style.visibility = 'visible'
+    }
+    if(buttons.length >= 3){
+        thirdButton.style.visibility = 'visible'
+    }
 }
