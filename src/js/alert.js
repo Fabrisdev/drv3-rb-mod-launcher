dbb6b5cce4f3ef14c9a147a74a599ddea9cd5c71
fabrisdev <ezepuntocom@hotmail.com>
2025-03-12 12:37:10 -0600

    Showing text

--- src/js/alert.js
+++ src/js/alert.js
@@ -1,5 +1,7 @@
 const alertImage = document.getElementById("alert-image")
+const alertText = document.getElementById('installation_started_text')
 
 export function showAlert({ text, buttons, isCancellable }){
     alertImage.style.visibility = 'visible'
+    alertText.innerHTML = text
 }
