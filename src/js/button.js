00a60f7e451245a266fd258757773f8ce46f384c
fabrisdev <ezepuntocom@hotmail.com>
2025-03-13 00:22:13 -0600

    Fixed event firing multiple times for each children

--- src/js/button.js
+++ src/js/button.js
@@ -4,13 +4,13 @@ import { playHoverSoundEffect, playSelectSoundEffect } from "./audio.js"
 export function button(element){
     const isClickable = true
 
-    element.addEventListener('mouseover', () => {
+    element.addEventListener('mouseenter', () => {
         if(!isClickable) return
         playHoverSoundEffect()
         element.classList.add('hovered')
     })
 
-    element.addEventListener('mouseout', () => {
+    element.addEventListener('mouseleave', () => {
         element.classList.remove('hovered')
     })
 
