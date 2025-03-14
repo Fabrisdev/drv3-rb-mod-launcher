e3ff3b530a1d86b435d37237b1dc1451e6dc53aa
fabrisdev <ezepuntocom@hotmail.com>
2025-03-13 19:19:34 -0600

    showAlert function now returns the buttons in an easy to access way

--- src/js/alert.js
+++ src/js/alert.js
@@ -13,29 +13,34 @@ export function showAlert({ text, buttons, isCancellable }){
     alertImage.classList.add('show_alert')
     alertText.innerHTML = text
     const controller = new AbortController()
+    const buttonsMapped = new Map()
     if(buttons.length === 1){
         buttonIfOnlyOne.setVisible(true)
         buttonIfOnlyOne.setText(buttons[0].text)
         buttonIfOnlyOne.onClick(buttons[0].onClick, controller)
         buttonIfOnlyOne.setClickable(buttons[0].isClickable ?? true)
+        buttonsMapped.set(buttons[0].text, buttonIfOnlyOne)
     }
     if(buttons.length > 1){
         firstButton.setVisible(true)
         firstButton.setText(buttons[0].text)
         firstButton.onClick(buttons[0].onClick, controller)
         firstButton.setClickable(buttons[0].isClickable ?? true)
+        buttonsMapped.set(buttons[0].text, firstButton)
     }
     if(buttons.length >= 2){
         secondButton.setVisible(true)
         secondButton.setText(buttons[1].text)
         secondButton.onClick(buttons[1].onClick, controller)
         secondButton.setClickable(buttons[1].isClickable ?? true)
+        buttonsMapped.set(buttons[1].text, secondButton)
     }
     if(buttons.length >= 3){
         thirdButton.setVisible(true)
         thirdButton.setText(buttons[2].text)
         thirdButton.onClick(buttons[2].onClick, controller)
         thirdButton.setClickable(buttons[2].isClickable ?? true)
+        buttonsMapped.set(buttons[2].text, thirdButton)
     }
     document.addEventListener("keyup", event => {
         if(event.key === "Escape" && isCancellable) {
@@ -44,6 +49,7 @@ export function showAlert({ text, buttons, isCancellable }){
             controller.abort()
         }
     }, { signal: controller.signal })
+    return buttonsMapped
 }
 
 export function hideAlert(){
