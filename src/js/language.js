7685442f98b5919e94b84d010cddcd07c8601a6e
fabrisdev <ezepuntocom@hotmail.com>
2025-07-04 18:36:06 -0300

    Added event for updating UI on language change

--- src/js/language.js
+++ src/js/language.js
@@ -1,6 +1,12 @@
 let language = 'en'
 
 export function setLanguage(newLanguage) {
+    const selectedLanguageEvent = new CustomEvent('language-selected', {
+        detail: {
+            language: newLanguage
+        }
+    })
+    window.dispatchEvent(selectedLanguageEvent)
     language = newLanguage
 }
 
