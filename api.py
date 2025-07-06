357f9726085dbd93ad793e1b751bedcc30fedfc6
fabrisdev <ezepuntocom@hotmail.com>
2025-07-06 18:18:57 -0300

    finished installing the mod translated

--- api.py
+++ api.py
@@ -34,6 +34,12 @@ class Api:
         try_delete_old_reloaded_configs()
         try_delete_old_mod_versions()
         extract_and_store_mod(mod_user_configured_path)
+        if language == 'en':
+            send_message_about_installation_status('Finished installing the mod. Creating shortcut...')
+        if language == 'es':
+            send_message_about_installation_status('Instalación del mod finalizada. Creando un atajo...')
+        if language == 'fr':
+            send_message_about_installation_status("Installation de mod terminée. Création d'un racourci...")
         if drv3_user_configured_path != "": create_shortcut(drv3_user_configured_path, mod_user_configured_path)
         else: create_shortcut(danganronpa_path, mod_user_configured_path)
         send_message_about_installation_status('All done! The mod has been installed. A launcher has been added on your desktop')
